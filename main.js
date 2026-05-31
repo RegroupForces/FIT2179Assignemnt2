39b66c67b769bb21ed25e8915e630fc5dd658046
RegroupForces <161395559+RegroupForces@users.noreply.github.com>
2026-05-31 11:58:48 +1000

    Added graph for 2003 and 2013

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-var vis1 = "Graphs/GDPDotMap.json";
-vegaEmbed('#PorpSymbolMap2023', vis1);
+var PorpSymbolMap2003 = "Graphs/GDPDotMap2003.json";
+vegaEmbed('#PorpSymbolMap2003', PorpSymbolMap2003);
+
+var PorpSymbolMap2013 = "Graphs/GDPDotMap2013.json";
+vegaEmbed('#PorpSymbolMap2013', PorpSymbolMap2013);
+
+var PorpSymbolMap2023 = "Graphs/GDPDotMap2023.json";
+vegaEmbed('#PorpSymbolMap2023', PorpSymbolMap2023);
 
 
 var foodImportVis = "foodImports.json";
